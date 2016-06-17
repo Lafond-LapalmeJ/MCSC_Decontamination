2f5687691fe299327a2878293eb26855d74a5bcb
duceppemo <marc-olivier.duceppe@inspection.gc.ca>
2016-06-17 16:02:02 -0400

    Syntax improvement
    To account potential spaces (and other special character) in file paths and names.

--- MCSC_decontamination.sh
+++ MCSC_decontamination.sh
@@ -21,8 +21,8 @@ FASTA=""
 # $LVL: clustering level (integer) 3 = 8 clusters, 4 = 16 clusters (default 32)
 LVL=5
 
-# $OUT: Directory to output the clusters (default PWD)
-OUT=$PWD
+# "${OUT}": Directory to output the clusters (default PWD)
+OUT="$PWD"
 
 # $UNIREF90: path to the DIAMOND UNIREF90 database (REQUIRED) 
 UNIREF90=""
@@ -30,10 +30,10 @@ UNIREF90=""
 # $UNIREF100: path to the UNIREF100 taxonomy list (REQUIRED)
 UNIREF100=""
 
-# $TAXDUMP: path to the NCBI taxonomy dump (REQUIRED)
+# "${TAXDUMP}": path to the NCBI taxonomy dump (REQUIRED)
 TAXDUMP=""
 
-# $MCSC: path to the MCSC_Decontamination folder (REQUIRED)
+# "${MCSC}": path to the MCSC_Decontamination folder (REQUIRED)
 MCSC=""
 
 # $TAXO_LVL: taxonomic level for the WR index (default: phylum)
@@ -50,9 +50,9 @@ T=8
 
 
 
-grep "=" $1 > $OUT/param_file.txt
+grep "=" "$1" > "${OUT}"/param_file.txt
 
-source $OUT/param_file.txt
+source "${OUT}"/param_file.txt
 
 rm param_file.txt
 
@@ -63,50 +63,50 @@ then
 	mkdir -p "$OUT"	
 
 	## get the fasta name
-	FILE=$(basename "$FASTA")
+	FILE="$(basename "$FASTA")"
 	NAME="${FILE%.*}"
 
 	## DIAMOND blast
-	if ! [ -e $OUT/$NAME.daa ]
+	if [ ! -f "${OUT}"/"${NAME}".daa ]
 	then
-		diamond blastx -d $UNIREF90 -q $FASTA -a $OUT/$NAME -t $OUT -p $T
+		diamond blastx -d "$UNIREF90" -q "$FASTA" -a "${OUT}"/"${NAME}" -t "${OUT}" -p "$T"
 	fi
 
 	## Extract DIAMOND blast taxonomy
-	perl $MCSC/scripts/daa_to_tagc.pl $UNIREF100 $OUT/${NAME}.daa
+	perl "${MCSC}"/scripts/daa_to_tagc.pl $UNIREF100 "${OUT}"/"${NAME}".daa
 	
 	## Format the DIAMOND output 
-	perl $MCSC/scripts/get_taxa_from_diamond.pl $OUT/$NAME.daa.tagc \
-	$TAXDUMP/names.dmp $TAXDUMP/nodes.dmp > $OUT/temp.txt
-	sort -rk3,3 $OUT/temp.txt | sort -uk1,1 > $OUT/taxo_uniq.txt  
-	sed "s/'\"//g" $OUT/taxo_uniq.txt > $OUT/temp.txt
-	mv $OUT/temp.txt $OUT/taxo_uniq.txt
+	perl "${MCSC}"/scripts/get_taxa_from_diamond.pl "${OUT}"/"${NAME}".daa.tagc \
+	"${TAXDUMP}"/names.dmp "${TAXDUMP}"/nodes.dmp > "${OUT}"/temp.txt
+	sort -rk3,3 "${OUT}"/temp.txt | sort -uk1,1 > "${OUT}"/taxo_uniq.txt  
+	sed "s/'\"//g" "${OUT}"/taxo_uniq.txt > "${OUT}"/temp.txt
+	mv "${OUT}"/temp.txt "${OUT}"/taxo_uniq.txt
 
 	## MCSC clusters name        
-	OUT_NAME=${OUT}/${NAME}_ 
+	OUT_NAME=""${OUT}"/"${NAME}"_"
 
 	## MCSC algorithm
-	$MCSC/scripts/mcsc_fix $FASTA $((LVL+1)) $OUT_NAME
+	"${MCSC}"/scripts/mcsc_fix "$FASTA" $((LVL+1)) "${OUT}"_NAME
 
 fi
 
-FILE=$(basename "$FASTA")
+FILE="$(basename "$FASTA")"
 NAME="${FILE%.*}"
 
 
 ## compute WR index and evaluate clusters 
-perl $MCSC/scripts/cluster_eval.pl $OUT $OUT/taxo_uniq.txt $TAXO_LVL $WHITE_NAME	
+perl "${MCSC}"/scripts/cluster_eval.pl "$OUT" "${OUT}"/taxo_uniq.txt "$TAXO_LVL" "$WHITE_NAME"	
 
 
 
 ## print a plot of the cluster evaluation
-R CMD BATCH --no-save --no-restore "--args $OUT/cluster_eval.tsv" $MCSC/scripts/clusters_evaluation.R $OUT/clusters_evaluation.Rout
+R CMD BATCH --no-save --no-restore "--args "${OUT}"/cluster_eval.tsv" "${MCSC}"/scripts/clusters_evaluation.R "${OUT}"/clusters_evaluation.Rout
 
-FILES=$(grep -o "\w*.fasta" $OUT/clusters_evaluation.Rout)
-echo $FILES
+FILES="$(grep -o "\w*.fasta" "${OUT}"/clusters_evaluation.Rout)"
+echo "$FILES"
 
-cat $FILES > $OUT/${NAME}_decont.fasta
+cat "$FILES" > "${OUT}"/"${NAME}"_decont.fasta
 
-mkdir $OUT/clusters
-mv $OUT/*cluster_*.fasta $OUT/clusters/
+mkdir "${OUT}"/clusters
+mv "${OUT}"/*cluster_*.fasta "${OUT}"/clusters/
 
